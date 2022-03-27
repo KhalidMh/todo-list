ea17861b2dc98093caacc67c4199e8ada00f291d
Khalid <kmarhoum@numericway.net>
2022-03-27 17:40:31 +0000

    initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Item::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name' => 'required|string|max:255|min:1'
+        ]);
+
+        $item = new Item();
+        $item->fill($request->only(['name']))->save();
+
+        return response(['Success' => 'Item created succesfuly!']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item $item)
+    {
+        return $item;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Item $item)
+    {
+        $this->validate($request,[
+            'completed' => 'required|boolean',
+        ]);
+
+        $item->update([
+            'completed' => $request->completed,
+            'completed_at' => $request->completed ? now() : null
+        ]);
+
+        return response(['Success' => 'Item updated succesfuly!']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item $item)
+    {
+        $item->delete();
+
+        return response(['Success' => 'Item deleted succesfuly!']);
+    }
+}
